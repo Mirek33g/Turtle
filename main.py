c2f5fddc4ee348265b0c859e66520c95e06ee4a8
Mirek33g <133931969+Mirek33g@users.noreply.github.com>
2023-06-30 20:07:17 +0000

    Comments

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ tim = t.Turtle()
 b = 3
 colors = ["red", "blue", "green", "purple", "orange", "yellow", "black"]
 
-
+# creates first turtles move 
 def draw(a):
   for i in range(a):
     tim.forward(50)
     tim.right(360 / a)
 
-
+#creates turtles move as many times as user wants
 for i in range(b, 12):
   tim.color(random.choice(colors))
   draw(b)
